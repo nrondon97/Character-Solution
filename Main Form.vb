9ca58aeb60b72561de98e58b1b40b03686360180
nrondon97 <150634151+nrondon97@users.noreply.github.com>
2024-08-09 21:34:08 -0400

    Add files via upload

---
+++ Main Form.vb
@@ -0,0 +1,13 @@
+﻿Public Class Characters
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        Me.Close()
+    End Sub
+End Class
